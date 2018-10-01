0c57c62034afae9532908d2f0c9a478c421b27cb
Shantanu Sikdar <radnahs@users.noreply.github.com>
2018-10-01 16:47:22 +0200

    format
    format changes

--- src/main/java/com/webtual/codeauditor/sast/checkmarx/ReDoSFromRegexInjection.java
+++ src/main/java/com/webtual/codeauditor/sast/checkmarx/ReDoSFromRegexInjection.java
@@ -115,240 +115,114 @@ public class ReDoSFromRegexInjection {
 		 */
 
 	}
-
-
-
 }
-
-
 
 class DummyRequest {
-
-
-
 	private Map<String, String> mapRequest = new HashMap<>();
 
-
-
 	public void setAttribute(String key, String value) {
-
 		mapRequest.put(key, value);
-
 	}
-
-
 
 	public String getParameter(String key) {
-
 		return mapRequest.get(key);
-
 	}
-
-
-
 }
-
-
 
 // https://rules.sonarsource.com/java/tag/owasp/RSPEC-2631
-
 class NonCompliantReDoSFromRegexInjection {
-
-
-
 	// BAD: Unsanitized user input is used to construct a regular expression
-
 	// Enables attackers to force the web server to evaluate
-
 	// regex such as "(a+)+" on inputs such as "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa!"
-
 	public boolean nonCompliantReDosCodeExample(DummyRequest request) {
-
 		String regex = request.getParameter("regex");
-
 		String input = request.getParameter("input");
-
-
-
 		return input.matches(regex); // Noncompliant
-
 	}
-
-
-
 }
-
 
 
 // https://help.semmle.com/wiki/display/CSHARP/Regular+expression+injection
-
 class CompliantReDoSFromRegexInjection {
 
-
-
 	// GOOD: User input is sanitized before constructing the regex
-
 	public boolean compliantCodeEg1(DummyRequest request) {
-
 		String input = request.getParameter("input");
-
-
-
 		return input.matches("\\d+"); // Compliant - use a safe hardcoded regex
-
 	}
-
-
 
 	public boolean compliantCodeEg2(DummyRequest request) {
-
 		String input = request.getParameter("input");
-
 		String regex = request.getParameter("regex");
-
-
-
 		Pattern p = Pattern.compile(regex);
-
 		Matcher m = p.matcher(input);
-
 		while (m.find()) {
-
 			System.out.println(input.substring(m.start(), m.end()));
-
 		}
-
 		return true;
-
 	}
-
-	
-
 }
 
-
-
 //https://referencesource.microsoft.com/#System/regex/system/text/regularexpressions/RegexParser.cs,bf0364a806ccb693
-
 class CompliantReDoSFromRegexInjectionDotNetWay{
-
 	private static byte Q = 5;    // quantifier
-
 	private static byte S = 4;    // ordinary stoppper
-
 	private static byte Z = 3;    // ScanBlank stopper
-
 	private static byte X = 2;    // whitespace
-
 	private static byte E = 1;    // should be escaped
 
-
-
 	private static byte[] _category = new byte[] {
-
                0,0,0,0,0,0,0,0,0,X,X,0,X,X,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-
                X,0,0,Z,S,0,0,0,S,S,Q,Q,0,0,S,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,Q,
-
                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,S,S,0,S,0,
-
                0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,Q,S,0,0,0};
 
-	
-
 	public static String escapeRegEx(String inputStr) {
-
 		char[] input = inputStr.toCharArray();
-
 		for (int i = 0; i < input.length; i++) {
-
 			if (isMetachar(input[i])) {
-
 				StringBuilder sb = new StringBuilder();
-
 				char ch = input[i];
-
 				int lastpos;
-
 				sb.append(input, 0, i);
-
 				do {
-
 					sb.append('\\');
-
 					switch (ch) {
-
 					case '\n':
-
 						ch = 'n';
-
 						break;
-
 					case '\r':
-
 						ch = 'r';
-
 						break;
-
 					case '\t':
-
 						ch = 't';
-
 						break;
-
 					case '\f':
-
 						ch = 'f';
-
 						break;
-
 					}
-
 					sb.append(ch);
-
 					i++;
-
 					lastpos = i;
-
 					while (i < input.length) {
-
 						ch = input[i];
-
 						if (isMetachar(ch)) {
-
 							break;
-
 						}
-
 						i++;
-
 					}
-
 					sb.append(input, lastpos, i - lastpos);
 
 				} while (i < input.length);
-
-
-
 				return sb.toString();
-
 			}
-
 		}
-
 		return new String(input);
-
 	}
-
 
 
 	private static boolean isMetachar(char ch) {
-
 		return (ch <= '|' && _category[ch] >= E);
-
 	}
-
-
-
 }
 
